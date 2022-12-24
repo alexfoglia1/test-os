66b3067a3bb35e39359e89391c2b81486fb545a5
Alex Foglia <alex.f1793@gmail.com>
2022-12-24 05:00:17 +0100

    Updated configure.sh

--- configure.sh
+++ configure.sh
@@ -11,7 +11,7 @@ echo "Installing build script in ${BUILDROOT}..."
 
 echo "$TARGET-as ../boot.s -o boot.o" > $BUILDROOT/build.sh
 echo "$TARGET-gcc -c ../kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra" >> $BUILDROOT/build.sh
-echo "$TARGET-gcc -T linker.ld -o $OSNAME.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc" >> $BUILDROOT/build.sh
+echo "$TARGET-gcc -T ../linker.ld -o $OSNAME.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc" >> $BUILDROOT/build.sh
 chmod +x $BUILDROOT/build.sh
 
 echo "Build script created in ${BUILDROOT}/build.sh"
